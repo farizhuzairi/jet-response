5852b46b0a5070fc31769d7d58fd4987c3ea6f90
Fariz Huzairi <farizhuzairi@gmail.com>
2025-03-27 13:50:40 +0700

    update

--- ResponseService.php
+++ ResponseService.php
@@ -2,8 +2,8 @@
 
 namespace Jet\Response;
 
-use Jet\Response\Http\CallHttpStatus;
 use Illuminate\Http\JsonResponse;
+use Jet\Response\Http\CallHttpStatus;
 use Jet\Response\Http\Resources\JetResource;
 use Symfony\Component\HttpFoundation\Response as BaseResponse;
 
@@ -11,11 +11,6 @@ abstract class ResponseService
 {
     use CallHttpStatus;
 
-    /**
-     * Data objek yang harus tersedia
-     * untuk diinisiasi
-     * 
-     */
     protected JetResource $resource;
     protected array $headers = [];
     protected array $meta = [];
@@ -31,11 +26,6 @@ abstract class ResponseService
         $this->setStatusResponse($statusCode);
     }
 
-    /**
-     * Membangun objek secara manual
-     * 
-     * @return void
-     */
     protected function makeResource(): void
     {
         $this->resource = (new JetResource(
@@ -47,21 +37,11 @@ abstract class ResponseService
         ));
     }
 
-    /**
-     * Menyetel status respon berdasarkan kode status
-     * 
-     * @return void
-     */
     protected function setStatusResponse(int $statusCode = 0): void
     {
         $this->successful = $statusCode >= 200 && $statusCode <= 299 ? true : false;
     }
 
-    /**
-     * Set response status
-     * 
-     * @return static
-     */
     public function setStatusCode(int $statusCode): static
     {
         $this->statusCode = (int) $statusCode;
@@ -69,11 +49,6 @@ abstract class ResponseService
         return $this;
     }
 
-    /**
-     * Menyediakan informasi dan identitas pada http header
-     * 
-     * @return \Illuminate\Http\JsonResponse|\Symfony\Component\HttpFoundation\Response
-     */
     protected function makeHeaders(JsonResponse|BaseResponse $result): JsonResponse|BaseResponse
     {
         if(!empty($this->headers)){
@@ -84,33 +59,18 @@ abstract class ResponseService
         return $result;
     }
 
-    /**
-     * Set message data response
-     * 
-     * @return static
-     */
     public function setMessage(string $message): static
     {
         $this->message = (string) $message;
         return $this;
     }
 
-    /**
-     * Set http header
-     * 
-     * @return static
-     */
     public function setHeaders(array $headers): static
     {
         $this->headers = (array) $headers;
         return $this;
     }
 
-    /**
-     * Add http header
-     * 
-     * @return static
-     */
     public function addHeader(array|string $headers, array|string|null $value = null): static
     {
         if(is_array($headers)) {
@@ -124,22 +84,12 @@ abstract class ResponseService
         return $this;
     }
 
-    /**
-     * Set Metadata
-     * 
-     * @return static
-     */
     public function setMeta(array $meta): static
     {
         $this->meta = (array) $meta;
         return $this;
     }
 
-    /**
-     * Add Metadata
-     * 
-     * @return static
-     */
     public function addMeta(array|string $meta, array|string|null $value): static
     {
         if(is_array($meta)) {
@@ -153,11 +103,6 @@ abstract class ResponseService
         return $this;
     }
 
-    /**
-     * add Data
-     * 
-     * @return static
-     */
     public function addData(array|string $data, array|string|null $value = null): static
     {
         if(is_array($data)) {
@@ -171,11 +116,12 @@ abstract class ResponseService
         return $this;
     }
 
-    /**
-     * Mengembalikan objek Response
-     * 
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
+    public function author(array $author): static
+    {
+        $this->author = $author;
+        return $this;
+    }
+
     public function json(): BaseResponse
     {
         $this->setStatusResponse($this->statusCode);
@@ -189,31 +135,19 @@ abstract class ResponseService
                 'results' => $this->data,
             ],
             'meta' => $this->meta,
-            'author' => $this->author // is included while the value is empty by default
+            'author' => $this->author
         ])
         ->setStatusCode($this->statusCode);
 
         return $this->makeHeaders($result);
     }
 
-    /**
-     * Mengembalikan objek JsonResponse
-     * melalui data array
-     * .................................
-     * string $type = 'json' is Default
-     * .................................
-     * berasal dari inisiasi GateResponse::class 
-     * 
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function send(): JsonResponse
     {
         $this->makeResource();
 
-        $result = $this->resource;
-
-        $result = $result
-        ->additional(['author' => $this->author]); // is included while the value is empty by default
+        $result = $this->resource
+        ->additional(['author' => $this->author]);
 
         $result = $result
             ->response()
